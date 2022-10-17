aa842931687463cded4b733fbaebbd604a38c716
Kefiloe Moshoaliba <90796051+Moshoaliba@users.noreply.github.com>
2022-10-17 23:59:10 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace Hotel_Bookings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rb5Star = new System.Windows.Forms.RadioButton();
+            this.rb3Star = new System.Windows.Forms.RadioButton();
+            this.rbBasic = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.lbOutput = new System.Windows.Forms.ListBox();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.lblDisplay2 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtInput2 = new System.Windows.Forms.TextBox();
+            this.btnBook = new System.Windows.Forms.Button();
+            this.btnBookAgain = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbBasic);
+            this.groupBox1.Controls.Add(this.rb3Star);
+            this.groupBox1.Controls.Add(this.rb5Star);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(187, 141);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Hotels";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox6);
+            this.groupBox2.Controls.Add(this.checkBox5);
+            this.groupBox2.Controls.Add(this.checkBox4);
+            this.groupBox2.Controls.Add(this.checkBox3);
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(222, 41);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(220, 198);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Preferences";
+            // 
+            // rb5Star
+            // 
+            this.rb5Star.AutoSize = true;
+            this.rb5Star.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb5Star.Location = new System.Drawing.Point(6, 34);
+            this.rb5Star.Name = "rb5Star";
+            this.rb5Star.Size = new System.Drawing.Size(75, 21);
+            this.rb5Star.TabIndex = 0;
+            this.rb5Star.TabStop = true;
+            this.rb5Star.Text = "5 STAR";
+            this.rb5Star.UseVisualStyleBackColor = true;
+            // 
+            // rb3Star
+            // 
+            this.rb3Star.AutoSize = true;
+            this.rb3Star.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb3Star.Location = new System.Drawing.Point(6, 61);
+            this.rb3Star.Name = "rb3Star";
+            this.rb3Star.Size = new System.Drawing.Size(75, 21);
+            this.rb3Star.TabIndex = 1;
+            this.rb3Star.TabStop = true;
+            this.rb3Star.Text = "3 STAR";
+            this.rb3Star.UseVisualStyleBackColor = true;
+            // 
+            // rbBasic
+            // 
+            this.rbBasic.AutoSize = true;
+            this.rbBasic.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbBasic.Location = new System.Drawing.Point(6, 88);
+            this.rbBasic.Name = "rbBasic";
+            this.rbBasic.Size = new System.Drawing.Size(65, 21);
+            this.rbBasic.TabIndex = 2;
+            this.rbBasic.TabStop = true;
+            this.rbBasic.Text = "BASIC";
+            this.rbBasic.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(6, 37);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(77, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Balcony";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(6, 64);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(201, 21);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "1 Bathroom and 1 Bedroom";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.Location = new System.Drawing.Point(6, 93);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(208, 21);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "2 Bedrooms and 1 Bathroom";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox4.Location = new System.Drawing.Point(6, 120);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(215, 21);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "3 Bedrooms and 2 Bathrooms";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox5.Location = new System.Drawing.Point(6, 147);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(215, 21);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "4 Bedrooms and 2 Bathrooms";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox6.Location = new System.Drawing.Point(6, 174);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(120, 21);
+            this.checkBox6.TabIndex = 2;
+            this.checkBox6.Text = "Swimming pool";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // lbOutput
+            // 
+            this.lbOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbOutput.FormattingEnabled = true;
+            this.lbOutput.ItemHeight = 16;
+            this.lbOutput.Location = new System.Drawing.Point(487, 37);
+            this.lbOutput.Name = "lbOutput";
+            this.lbOutput.Size = new System.Drawing.Size(280, 196);
+            this.lbOutput.TabIndex = 2;
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.Location = new System.Drawing.Point(9, 261);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(169, 17);
+            this.lblDisplay.TabIndex = 3;
+            this.lblDisplay.Text = "Bookings for which day? :";
+            // 
+            // lblDisplay2
+            // 
+            this.lblDisplay2.AutoSize = true;
+            this.lblDisplay2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay2.Location = new System.Drawing.Point(12, 289);
+            this.lblDisplay2.Name = "lblDisplay2";
+            this.lblDisplay2.Size = new System.Drawing.Size(234, 17);
+            this.lblDisplay2.TabIndex = 4;
+            this.lblDisplay2.Text = "How long will you be staying(Days) :";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(195, 261);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(127, 23);
+            this.txtInput.TabIndex = 5;
+            // 
+            // txtInput2
+            // 
+            this.txtInput2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput2.Location = new System.Drawing.Point(253, 289);
+            this.txtInput2.Name = "txtInput2";
+            this.txtInput2.Size = new System.Drawing.Size(115, 23);
+            this.txtInput2.TabIndex = 6;
+            // 
+            // btnBook
+            // 
+            this.btnBook.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBook.Location = new System.Drawing.Point(487, 249);
+            this.btnBook.Name = "btnBook";
+            this.btnBook.Size = new System.Drawing.Size(89, 29);
+            this.btnBook.TabIndex = 7;
+            this.btnBook.Text = "Book";
+            this.btnBook.UseVisualStyleBackColor = true;
+            this.btnBook.Click += new System.EventHandler(this.btnBook_Click);
+            // 
+            // btnBookAgain
+            // 
+            this.btnBookAgain.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBookAgain.Location = new System.Drawing.Point(596, 248);
+            this.btnBookAgain.Name = "btnBookAgain";
+            this.btnBookAgain.Size = new System.Drawing.Size(78, 47);
+            this.btnBookAgain.TabIndex = 8;
+            this.btnBookAgain.Text = "Book again";
+            this.btnBookAgain.UseVisualStyleBackColor = true;
+            this.btnBookAgain.Click += new System.EventHandler(this.btnBookAgain_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(687, 248);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(80, 31);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(19, 323);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(60, 17);
+            this.lblTotal.TabIndex = 10;
+            this.lblTotal.Text = "Total  :";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(99, 323);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(100, 23);
+            this.txtOutput.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(287, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "HOTEL BOOKINGS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnBookAgain);
+            this.Controls.Add(this.btnBook);
+            this.Controls.Add(this.txtInput2);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblDisplay2);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.lbOutput);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbBasic;
+        private System.Windows.Forms.RadioButton rb3Star;
+        private System.Windows.Forms.RadioButton rb5Star;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.ListBox lbOutput;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Label lblDisplay2;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtInput2;
+        private System.Windows.Forms.Button btnBook;
+        private System.Windows.Forms.Button btnBookAgain;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿//Moshoaliba,K-34316345
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotel_Bookings
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBook_Click(object sender, EventArgs e)
+        {
+            const decimal five_Star = 2000.00m, three_Star = 1300.00m, basic = 500.00m;
+            decimal total = 0m;
+            decimal price = 0m;
+            decimal total_price = 0m;
+            string day = " ";
+
+            day = txtInput.Text;
+
+            if (rb5Star.Checked)
+            {
+                lbOutput.Items.Add("5 STAR hotel");
+                total += five_Star;
+            }
+            else if(rb3Star.Checked)
+            {
+                lbOutput.Items.Add("3 Star hotel");
+                total += three_Star;
+            }
+            else if(rbBasic.Checked)
+            {
+                lbOutput.Items.Add("Basic hotel");
+                total += basic;
+            }
+            else
+            {
+                lbOutput.Items.Add(" ");
+            }
+
+            if(checkBox1.Checked)
+            {
+                lbOutput.Items.Add("Balcony");
+                price += 350;
+            }
+            if(checkBox2.Checked)
+            {
+                lbOutput.Items.Add("1 Bathroom & 1 Bedroom");
+                price += 250;
+            }
+            if(checkBox3.Checked)
+            {
+                lbOutput.Items.Add("2 Bedrooms & 1 Bathroom");
+                price += 350;
+            }
+            if(checkBox4.Checked)
+            {
+                lbOutput.Items.Add("3 Bedrooms & 2 Bathrooms");
+                price += 450;
+            }
+            if(checkBox5.Checked)
+            {
+                lbOutput.Items.Add("4 Bedrooms & 2 Bathrooms");
+                price += 550;
+            }
+            if(checkBox6.Checked)
+            {
+                lbOutput.Items.Add("Swimming pool");
+                price += 400;
+            }
+
+            switch(day)
+            {
+                case "Monday":
+                    MessageBox.Show("WE OPEN FROM 09:00 - 14:00 for Bookings");
+                    break;
+                case "Tuesday":
+                    MessageBox.Show("WE OPEN FROM 09:00 - 14:00 for Bookings");
+                    break;
+                case "Wednesday":
+                    MessageBox.Show("WE OPEN FROM 09:30 - 13:45 for Bookings");
+                    break;
+                case "Thursday":
+                    MessageBox.Show("WE OPEN FROM 09:30 - 13:30 for Bookings");
+                    break;
+                case "Friday":
+                    MessageBox.Show("WE OPEN FROM 09:00 - 16:00 for Bookings");
+                    break;
+                default:
+                    MessageBox.Show("WE DONT DO BOOKINGS ON THIS DAY");
+                    break;
+            }
+
+            if(int.TryParse(txtInput2.Text,out int days))
+            {
+                lbOutput.Items.Add("FOR " + days.ToString() + " DAYS");
+                total_price += (total + price) * days;
+                txtOutput.Text = "R " + total_price.ToString();
+            }
+            else
+            {
+                MessageBox.Show("INVALID INPUT FORMAT!!!");
+            }
+            this.Focus();
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnBookAgain_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = "";
+            txtInput2.Text = "";
+            txtOutput.Text = "";
+            lbOutput.Items.Clear();
+        }
+    }
+}
